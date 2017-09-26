9d93cfbe8104d1486a142edbd29f6b2c2d8ab102
Flytoo <1906457616@qq.com>
2017-09-26 14:14:19 +0800

    updated ViewController code

--- TreeTableViewDemo/ViewController.swift
+++ TreeTableViewDemo/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 	@IBOutlet weak var tableView: UITableView!
-
+	
 	var dataSource = TreeNode.mockData()
 	
 	override func viewDidLoad() {
@@ -19,44 +19,43 @@ class ViewController: UIViewController {
 		self.automaticallyAdjustsScrollViewInsets = false
 		
 		tableView.tableFooterView = UIView()
-				
+		
 		tableView.register(TreeNodeCell.self, forCellReuseIdentifier: "TreeNodeCell")
 		tableView.register(TreeLeafCell.self, forCellReuseIdentifier: "TreeLeafCell")
 	}
 }
 
 extension ViewController: UITableViewDelegate{
-func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-	tableView.deselectRow(at: indexPath, animated: true)
-	
-	let node = dataSource[indexPath.row]
-	let subs = node.subNodes
-	if subs.isEmpty {
-		return
-	}
-	
-	node.isOpen = !node.isOpen
-	let insertIndex = dataSource.index(of: node)! + 1
-	if node.isOpen {
-		dataSource.insert(contentsOf: subs, at: insertIndex)
-		tableView.insertRows(at: subs.map{
-			IndexPath(row: dataSource.index(of: $0)!, section: 0)
-		}, with: .top)
-	}
-	else{
-		for subNode in node.descendantNodes {
-			guard let index = dataSource.index(of: subNode) else {
-				continue
-			}
-			subNode.isOpen = false
-			dataSource.remove(at: index)
-			tableView.deleteRows(at: 
-				[IndexPath(row: index, section: 0)]
-			, with: .bottom)
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+		
+		let node = dataSource[indexPath.row]
+		if node.isLeaf {
+			return
 		}
+		
+		node.isOpen = !node.isOpen
+		let nodes = node.needsDisplayNodes
+		let insertIndex = dataSource.index(of: node)! + 1
+		if node.isOpen {
+			dataSource.insert(contentsOf: nodes, at: insertIndex)
+			tableView.insertRows(at: nodes.map{
+				IndexPath(row: dataSource.index(of: $0)!, section: 0)
+			}, with: .top)
+		}
+		else{
+			for subNode in nodes {
+				guard let index = dataSource.index(of: subNode) else {
+					continue
+				}
+				dataSource.remove(at: index)
+				tableView.deleteRows(at: 
+					[IndexPath(row: index, section: 0)]
+					, with: .bottom)
+			}
+		}
+		tableView.reloadRows(at: [indexPath], with: .none)
 	}
-	tableView.reloadRows(at: [indexPath], with: .none)
-}
 }
 
 extension ViewController: UITableViewDataSource{
@@ -64,22 +63,23 @@ extension ViewController: UITableViewDataSource{
 		return dataSource.count 
 	}
 	
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-	let node = dataSource[indexPath.row]
-	let subs = node.subNodes
-	let flagText = !subs.isEmpty ? (node.isOpen ? "-" : "+") : ""
-	
-	var cell: UITableViewCell!
-	if subs.isEmpty {
-		cell = tableView.dequeueReusableCell(withIdentifier: "TreeLeafCell", for: indexPath) as! TreeLeafCell
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let node = dataSource[indexPath.row]
+		let flagText = !node.isLeaf ? (node.isOpen ? "-" : "+") : ""
+		
+		var cell: UITableViewCell!
+		if node.isLeaf {
+			cell = tableView.dequeueReusableCell(withIdentifier: "TreeLeafCell", for: indexPath) as! TreeLeafCell
+		}
+		else{
+			cell = tableView.dequeueReusableCell(withIdentifier: "TreeNodeCell", for: indexPath) as! TreeNodeCell
+		}
+		
+		cell.textLabel?.text = String(repeating: "    ", 
+		                              count: (node.level > 1 ? node.level : 0)) 
+			+ "\(flagText)  " + node.name
+		
+		return cell
 	}
-	else{
-		cell = tableView.dequeueReusableCell(withIdentifier: "TreeNodeCell", for: indexPath) as! TreeNodeCell
-	}
-	
-	cell.textLabel?.text = String(repeating: "    ", count: node.level) + "\(flagText)  " + node.name
-	
-	return cell
-}
 }
 
